b1ef2b42c7a804805dd4de3a22787d48fc3bc21c
Sotiris Spyrou <sotirisspyrou@gmail.com>
2025-08-10 18:37:36 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""
+MarTech Integration Hub
+
+Universal marketing technology integration & workflow automation platform.
+Eliminates marketing data silos and creates unified marketing operations.
+"""
+
+__version__ = "1.0.0"
+__author__ = "MarTech Integration Hub Team"
+__description__ = "Universal marketing technology integration platform"
+
+from .integration_manager import IntegrationManager
+
+__all__ = ["IntegrationManager"]
